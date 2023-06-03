2a8d5851b55be83f16ba46a29cee959506596117
bartek <bartek@grupazpr.pl>
2023-06-03 09:32:48 +0200

    extracted LocationItem + grid added

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,51 @@
 <template>
   <div id="app">
+    <div class="cell cell-location">
+      <LocationItem :location="weather.location"></LocationItem>
+    </div>
     <div class="cell cell-map">
-      <MapContainer></MapContainer>
+      <MapItem></MapItem>
     </div>
+    <div class="cell cell-current">
+      <h2>Current</h2>
+    </div>
+    <div class="cell cell-daily">Daily</div>
 
-    <div v-bind:id="1">
-      <h2>{{ weather.location.name }}</h2>
-      <p>{{ weather.location.country }}</p>
-      <p>{{ weather.location.latitude }}</p>
-      <p>{{ weather.location.longitude }}</p>
-      <p  >{{ weather.location.timezone }}</p>
-    </div>
-    <div v-bind:id="2">
-      <p>{{ weather.current.temp_c }}</p>
-      <p>{{ weather.current.wind_kph }}</p>
-      <p>{{ weather.current.wind_dir }}</p>
-      <p>{{ weather.current.preasure_mb }}</p>
-      <p>{{ weather.current.precip_mm }}</p>
-      <p>{{ weather.current.humidity }}</p>
-      <p>{{ weather.current.condition }}</p>
-    </div>
-    <div v-for="day in weather.daily" v-bind:key="day.date">
-      <p>{{ day.date }}</p>
-      <p>{{ day.maxtemp_c }}</p>
-      <p>{{ day.mintemp_c }}</p>
-      <p>{{ day.avgtemp_c }}</p>
-      <p>{{ day.maxwind_kph }}</p>
-      <p>{{ day.totalprecip_mm }}</p>
-      <p>{{ day.avghumidity }}</p>
-      <p>{{ day.avghumidity }}</p>
-      <p>{{ day.condition }}</p>
-    </div>
+    <!--    <div v-bind:id="2">-->
+    <!--      <p>{{ weather.current.temp_c }}</p>-->
+    <!--      <p>{{ weather.current.wind_kph }}</p>-->
+    <!--      <p>{{ weather.current.wind_dir }}</p>-->
+    <!--      <p>{{ weather.current.preasure_mb }}</p>-->
+    <!--      <p>{{ weather.current.precip_mm }}</p>-->
+    <!--      <p>{{ weather.current.humidity }}</p>-->
+    <!--      <p>{{ weather.current.condition }}</p>-->
+    <!--    </div>-->
+    <!--    <div v-for="day in weather.daily" v-bind:key="day.date">-->
+    <!--      <p>{{ day.date }}</p>-->
+    <!--      <p>{{ day.maxtemp_c }}</p>-->
+    <!--      <p>{{ day.mintemp_c }}</p>-->
+    <!--      <p>{{ day.avgtemp_c }}</p>-->
+    <!--      <p>{{ day.maxwind_kph }}</p>-->
+    <!--      <p>{{ day.totalprecip_mm }}</p>-->
+    <!--      <p>{{ day.avghumidity }}</p>-->
+    <!--      <p>{{ day.avghumidity }}</p>-->
+    <!--      <p>{{ day.condition }}</p>-->
+    <!--    </div>-->
   </div>
 </template>
 
 <script>
-import MapContainer from './components/MapContainer.vue'
+import MapItem from "./components/MapItem.vue";
+import LocationItem from "./components/LocationItem.vue";
 
 export default {
-  name: 'App',
-  components: {
-    MapContainer
-  },
+  name: "App",
+  components: { MapItem, LocationItem },
   data() {
     return {
       weather: {
-        location: {},
-        current: {},
-        daily: []
+        type: Object,
+        required: true,
       },
     };
   },
@@ -70,37 +68,54 @@ export default {
 </script>
 
 <style>
-  html, body {
-    height: 100%;
-    margin: 0;
-  }
+html,
+body {
+  height: 100%;
+  margin: 0;
+}
 
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    height: 100%;
-    display: grid;
-    grid-template-columns: 100vh;
-    grid-auto-rows: 1fr;
-    grid-gap: 1rem;
-    padding: 1rem;
-    box-sizing: border-box;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 100vh;
+  grid-auto-rows: 1fr;
+  grid-gap: 1rem;
+  padding: 1rem;
+  box-sizing: border-box;
+}
 
-  .cell {
-    border-radius: 4px;
-    background-color: lightgrey;
-  }
+.cell {
+  border-radius: 4px;
+  background-color: lightgrey;
+}
 
-  .cell-map {
-    grid-column: 1;
-    grid-row-start: 1;
-    grid-row-end: 3;
-  }
+.cell-map {
+  grid-column: 1;
+  grid-row: 2;
+}
+
+.cell-location {
+  grid-column: 3;
+  grid-row: 2;
+}
+
+.cell-current {
+  grid-column: 2;
+  grid-row: 2;
+}
+
+.cell-daily {
+  grid-column: 1 / 4;
+  grid-row: 1;
+}
+
 h2 {
   margin: 40px 0 0;
 }
+
 div {
   display: inline-block;
-  margin: 0 10px;
+  margin: 0 0;
 }
 </style>
---
+++ src/components/LocationItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="location__container">
+      <h2>{{ location.name }}</h2>
+      <p>{{ location.country }}</p>
+      <p>{{ location.latitude }}</p>
+      <p>{{ location.longitude }}</p>
+      <p>{{ location.timezone }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "LocationItem",
+  props: {
+    location: {
+      type: Object,
+      required: true
+    }
+  },
+  // computed: {}
+};
+</script>
+
+<style scoped>
+
+</style>
