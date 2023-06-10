1a4dd73ee41b39783a1314b2956693c95ef7e721
bartek <bartek@grupazpr.pl>
2023-06-10 14:24:27 +0200

    the coordinates are displayed on the map

--- src/components/MapItem.vue
+++ src/components/MapItem.vue
@@ -1,12 +1,16 @@
 <template>
   <div ref="map-root" style="width: 100%; height: 100%"></div>
+  <div id="mouse-position"></div>
 </template>
 
 <script>
-import View from "ol/View";
 import Map from "ol/Map";
-import TileLayer from "ol/layer/Tile";
+import MousePosition from "ol/control/MousePosition.js";
 import OSM from "ol/source/OSM";
+import TileLayer from "ol/layer/Tile";
+import View from "ol/View";
+import { createStringXY } from "ol/coordinate.js";
+import { defaults as defaultControls } from "ol/control.js";
 import "ol/ol.css";
 
 export default {
@@ -14,7 +18,14 @@ export default {
   components: {},
   props: {},
   mounted() {
+    const mousePositionControl = new MousePosition({
+      coordinateFormat: createStringXY(4),
+      projection: "EPSG:4326",
+      // className: "custom-mouse-position",
+      // target: document.getElementById("mouse-position"),
+    });
     new Map({
+      controls: defaultControls().extend([mousePositionControl]),
       target: this.$refs["map-root"],
       layers: [
         new TileLayer({
